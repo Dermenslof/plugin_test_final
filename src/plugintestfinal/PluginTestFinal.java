a84d0e280d3d21e57b2fc425016dbd056bd174c4
Dermenslof <francois@montaron.eu>
2015-10-10 03:38:34 +0200

    test

---
+++ src/plugintestfinal/PluginTestFinal.java
@@ -0,0 +1,11 @@
+package plugintestfinal;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginTestFinal extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		getLogger().info("ok");
+	}
+}
